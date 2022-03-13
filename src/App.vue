a6dc9ba44168ceac47bd5004a103733ca934a64f
tinveo <111413265+tinveo@users.noreply.github.com>
2022-03-13 15:20:21 +0000

    Fix github link

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <div class="row">
+      <div class="col-xs-12">
+        <transition name="slide" mode="out-in">
+          <router-view></router-view>
+        </transition>
+      </div>
+    </div>
