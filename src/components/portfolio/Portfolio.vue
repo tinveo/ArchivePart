e308e41c69f9ea15c31521d5010ef7bcf4adb258
tinveo <111413265+tinveo@users.noreply.github.com>
2022-03-30 16:06:01 +0000

    update client

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -2,3 +2,19 @@
 <template>
   <div>
     <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import Stock from './Stock.vue';
+
+export default {
+  computed: {
+    ...mapGetters({
+      stocks: 'stockPortfolio'
+    })
+  },
+  components: {
+    appStock: Stock
+  }
