f3b24b89463e648c78c8c3beca0a9b5d167f7730
tinveo <111413265+tinveo@users.noreply.github.com>
2022-03-30 16:00:40 +0000

    resume update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,3 +4,16 @@
     <h1>Trade or View your Portfolio</h1>
     <h6>You may Save and Load your data</h6>
     <h6>Click on 'End Day' to begin a New Day!</h6>
+    <hr>
+    <p>Your funds: {{ funds | currency }}</p>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    funds() {
+      return this.$store.getters.funds;
+    }
+  }
+}
+</script>
---
+++ src/components/portfolio/Portfolio.vue
@@ -0,0 +1,4 @@
+
+<template>
+  <div>
+    <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
