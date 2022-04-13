f5f3b5f2d8b88b484c891fa32af1de3eaa437c79
tinveo <111413265+tinveo@users.noreply.github.com>
2022-04-13 09:21:00 +0000

    Remove duplication

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -2,4 +2,5 @@
   <div class="col-xs-12 col-sm-6 col-md-4">
     <div class="panel panel-success">
       <div class="panel-heading">
-        <h3 class="panel-title"
+        <h3 class="panel-title">
+        
