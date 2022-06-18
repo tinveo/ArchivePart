261c7d59b2e3cb3e06cea39569aaa942bfb0c746
tinveo <111413265+tinveo@users.noreply.github.com>
2022-06-18 22:26:24 +0000

    Update scheduler.xml

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -24,4 +24,4 @@
             class="btn btn-success"
             @click="buyStock"
             :disabled="insufficientFunds || quantity <= 0 || !Number.isInteger(quantity)"
-          >{{ insufficientFunds ? 
+          >{{ insufficientFunds ? 'Insuffi
