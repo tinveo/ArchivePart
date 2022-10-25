bc923d7c703840224c108d88163ea15265237806
tinveo <111413265+tinveo@users.noreply.github.com>
2022-10-25 18:47:43 +0000

    Fix CS/WS issues

---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div class="row">
+    <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
+  </div>
+</template>
+
+<script>
+import Stock from './Stock.vue';
+export default {
+  components: {
+    appStock: Stock
+  },
+  computed: {
+    stocks() {
+      return this.$store.getters['stocks'];
+    }
+  }
+}
