3f364f4f4d31af690feac654a23b09ee5079e9dc
tinveo <111413265+tinveo@users.noreply.github.com>
2022-12-31 13:04:01 +0000

    Bumped version to 2.0.1

--- src/store/modules/stocks.js
+++ src/store/modules/stocks.js
@@ -11,3 +11,14 @@ const mutations = {
   'SET_STOCKS'(state, stocks) {
     state.stocks = stocks;
   },
+  'RND_STOCKS'(state) {
+    state.stocks.forEach(stock => {
+      stock.price = Math.round(stock.price * (1 + Math.random()) - 0.5);
+    });
+  }
+};
+
+const actions = {
+  buyStock: ({ commit }, order) => {
+    commit('BUY_STOCK', order);
+  },
