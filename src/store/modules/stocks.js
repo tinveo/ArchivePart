d8693485c7425dd57d5c78c2a1f2f756cd743945
tinveo <111413265+tinveo@users.noreply.github.com>
2023-01-01 11:20:24 +0000

    l10n: Updated Italian translation

--- src/store/modules/stocks.js
+++ src/store/modules/stocks.js
@@ -22,3 +22,18 @@ const actions = {
   buyStock: ({ commit }, order) => {
     commit('BUY_STOCK', order);
   },
+  initStocks: ({ commit }) => {
+    commit('SET_STOCKS', stocks);
+  },
+  randomizeStocks: ({ commit }) => {
+    commit('RND_STOCKS');
+  }
+};
+
+const getters = {
+  stocks: state => {
+    return state.stocks;
+  }
+};
+
+export default { state, mutations, actions, getters };
