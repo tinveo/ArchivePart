0e480cd1390132d9455a35c446b41a4166ef3830
tinveo <111413265+tinveo@users.noreply.github.com>
2023-01-18 16:33:17 +0000

    Add level-data.json contents.

--- src/store/store.js
+++ src/store/store.js
@@ -2,4 +2,6 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import stocks from './modules/stocks';
-import portfolio from './modules/portfolio
+import portfolio from './modules/portfolio';
+
+import * as
