6fa08adcde8232c12a75b8ef7583295b604556f4
tinveo <111413265+tinveo@users.noreply.github.com>
2023-02-21 16:07:59 +0000

    local rename

--- webpack.config.js
+++ webpack.config.js
@@ -5,3 +5,15 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue',
+        options: {
+          // vue-loader options go here
+        }
